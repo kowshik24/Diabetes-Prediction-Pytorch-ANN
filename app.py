814f7f026f4b2f375a9a5d570b0df464966df93b
Kowshik Deb Nath <kowshikcseruet1998@gmail.com>
2023-07-31 04:06:28 +0600

    host updated on app.py file

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def predict():
     return render_template('index.html')  # render the form when the method is GET
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True,host="0.0.0.0" port=5000)
